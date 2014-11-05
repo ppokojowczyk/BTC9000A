139aac2380637e8346e4fd2272b9c42a91391a23
Piotr Pokojowczyk <piotrpokojowczyk@gmail.com>
2014-11-05 12:32:23 +0100

    First upload.

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-BTC9000A
-========
+BTC9000A Configuration Script
+=============================
 
-Linux config script for BTC 9000A keyboard.
+Linux config script for BTC 9000A PS/2 keyboard.
+
+Scancodes:
+==========
+
+* e05e - Shutdown
+* e05f - Sleep
+* e063 - Wake Up
+* e02e - Help
+* e020 - Favorites
+* e030 - WWW / HomePage
+* e026 - Shortcut
+* e032 - Search
+* e01e - Volume Up
+* e025 - Volume Down
+* e012 - Mute
+* e022 - Previous / FastBackward
+* e010 - Stop
+* e019 - Play/Pause
+* e021 - Eject
+* e024 - Next / FastForward
---
+++ btc9000a.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+#
+# Script by: Piotr Pokojowczyk
+# Contact: piotrpokojowczyk@gmail.com
+# Twitter: @pokojowczyk
+# Description: Multimedia keys config for BTC 9000A PS/2 keyboard.
+#
+
+# Power Down
+setkeycodes e05e 116
+
+# Suspend / Sleep
+setkeycodes e05f 205
+
+# Wake Up
+setkeycodes e063 143
+
+# Help
+setkeycodes e02e 138
+
+# Favorites
+setkeycodes e020 156
+
+# WWW
+setkeycodes e030 172
+
+# Shortcut
+setkeycodes e026 235
+
+# Search
+setkeycodes e032 217
+
+# Volume Up
+setkeycodes e01e 115
+
+# Volume Down
+setkeycodes e025 114
+
+# Mute
+setkeycodes e012 113
+
+# Previous
+setkeycodes e022 165
+
+# Stop
+setkeycodes e010 166
+
+# Play / Pause
+setkeycodes e019 164
+
+# Eject
+setkeycodes e021 161
+
+# Next
+setkeycodes e024 163
+
